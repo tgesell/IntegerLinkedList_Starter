146554b7a026c67b8445fa06dfdc51f989966bff
Tim Gesell <timothy.gesell@bvsd.org>
2019-09-24 11:09:57 -0600

    initial commit

---
+++ Node.java
@@ -0,0 +1,30 @@
+public class Node
+{
+    private int value;
+    private Node next;
+    public Node (int value)
+    {
+        this.value = value;
+        next = null;
+    }
+    
+    public int getValue()
+    {
+        return value;
+    }
+    
+    public Node getNext()
+    {
+        return next;
+    }
+    
+    public void setValue(int value)
+    {
+        this.value = value;
+    }
+    
+    public void setNext(Node next)
+    {
+        this.next = next;
+    }
+}
